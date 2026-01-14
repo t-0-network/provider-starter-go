9162fda0eaf91eb968eff15a663abbdc95b144e2
Anton Efimow <anton@efimow.name>
2026-01-14 13:56:13 +0100

    migrate to finalizePayout; upd proto to latest

--- template/internal/get_quote.go
+++ template/internal/get_quote.go
@@ -12,11 +12,9 @@ import (
 
 func GetQuote(ctx context.Context, networkClient paymentconnect.NetworkServiceClient) {
 	quote, err := networkClient.GetQuote(ctx, connect.NewRequest(&payment.GetQuoteRequest{
-		PayInCurrency: "EUR",
-		Amount: &payment.PaymentAmount{Amount: &payment.PaymentAmount_PayInAmount{
-			PayInAmount: &common.Decimal{Unscaled: 500, Exponent: 0}, // amount in EUR
+		Amount: &payment.PaymentAmount{Amount: &payment.PaymentAmount_SettlementAmount{
+			SettlementAmount: &common.Decimal{Unscaled: 500, Exponent: 0}, // amount in USD
 		}},
-		PayInMethod:    common.PaymentMethodType_PAYMENT_METHOD_TYPE_SEPA,
 		PayOutCurrency: "GBP",
 		PayOutMethod:   common.PaymentMethodType_PAYMENT_METHOD_TYPE_SWIFT,
 		QuoteType:      payment.QuoteType_QUOTE_TYPE_REALTIME,
@@ -24,14 +22,14 @@ func GetQuote(ctx context.Context, networkClient paymentconnect.NetworkServiceCl
 	if err != nil {
 		log.Printf("Error getting quote: %s\n", err.Error()) // handle errors appropriately
 		return
-	} else {
-		switch quote.Msg.Result.(type) {
-		case *payment.GetQuoteResponse_Success_:
-			log.Printf("Got success response with quote id: %d \n", quote.Msg.GetSuccess().QuoteId.QuoteId)
-		case *payment.GetQuoteResponse_Failure_:
-			log.Printf("Got failure response with reason: %s\n", quote.Msg.GetFailure().Reason.String())
-		default:
-			log.Println("Unknown type")
-		}
+	}
+
+	switch quote.Msg.Result.(type) {
+	case *payment.GetQuoteResponse_Success_:
+		log.Printf("Got success response with quote id: %d \n", quote.Msg.GetSuccess().QuoteId.QuoteId)
+	case *payment.GetQuoteResponse_Failure_:
+		log.Printf("Got failure response with reason: %s\n", quote.Msg.GetFailure().Reason.String())
+	default:
+		log.Println("Unknown type")
 	}
 }
