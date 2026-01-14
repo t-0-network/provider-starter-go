f3768cce2b4929396db786be7d86f59c15cd1c4b
Anton Efimow <anton@efimow.name>
2026-01-14 15:00:46 +0100

    migrate to finalizePayout; upd proto to latest (#5)
    * migrate to finalizePayout; upd proto to latest

--- template/internal/handler/payment.go
+++ template/internal/handler/payment.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"connectrpc.com/connect"
+	"github.com/t-0-network/provider-sdk-go/api/tzero/v1/common"
 	"github.com/t-0-network/provider-sdk-go/api/tzero/v1/payment"
 	"github.com/t-0-network/provider-sdk-go/api/tzero/v1/payment/paymentconnect"
 )
@@ -34,10 +35,26 @@ func (s *ProviderServiceImplementation) UpdatePayment(
 // TODO: Step 2.4 implement how you do payouts (payments initiated by your counterparts)
 func (s *ProviderServiceImplementation) PayOut(ctx context.Context, req *connect.Request[payment.PayoutRequest],
 ) (*connect.Response[payment.PayoutResponse], error) {
+
+	//TODO: FinalizePayout should be called when your system notifies that payout has been made successfully
+	_, err := s.networkClient.FinalizePayout(ctx, connect.NewRequest(&payment.FinalizePayoutRequest{
+		PaymentId: req.Msg.PaymentId,
+		Result: &payment.FinalizePayoutRequest_Success_{
+			Success: &payment.FinalizePayoutRequest_Success{
+				Receipt: &common.PaymentReceipt{
+					Details: &common.PaymentReceipt_Sepa_{
+						Sepa: &common.PaymentReceipt_Sepa{
+							BankingTransactionReferenceId: ref("123456"),
+						}},
+				},
+			}},
+	}))
+
+	if err != nil {
+		return nil, err
+	}
 	return connect.NewResponse(&payment.PayoutResponse{}), nil
 }
-
-//TODO: confirmPayout should be called when you system notifies that payout has been made successfully
 
 func (s *ProviderServiceImplementation) UpdateLimit(
 	ctx context.Context, req *connect.Request[payment.UpdateLimitRequest],
@@ -51,4 +68,13 @@ func (s *ProviderServiceImplementation) AppendLedgerEntries(
 ) (*connect.Response[payment.AppendLedgerEntriesResponse], error) {
 	// TODO: optionally implement handling of the notifications about new ledger transactions and new ledger entries
 	return connect.NewResponse(&payment.AppendLedgerEntriesResponse{}), nil
+}
+
+func (s *ProviderServiceImplementation) ApprovePaymentQuotes(ctx context.Context, c *connect.Request[payment.ApprovePaymentQuoteRequest]) (*connect.Response[payment.ApprovePaymentQuoteResponse], error) {
+	//TODO: this is the endpoint to have a last look at quote and approve after AML check is done
+	return connect.NewResponse(&payment.ApprovePaymentQuoteResponse{}), nil
+}
+
+func ref(s string) *string {
+	return &s
 }
